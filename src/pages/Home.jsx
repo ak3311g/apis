c0e5521de1cb5219aab20437319c7ee5a74f6afb
Ankit Kumar <ak3311g@gmail.com>
2023-12-17 17:34:11 +0530

    updatwe

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,6 @@ export default function Home() {
         <div className="pt-20">
             <Hub />
         </div>
-        <p className="bg-black text-white absolute bottom-0 left-1 p-3">
-          Sorry for inconvenience but some APIs are not working because of protocols issue
-          they uses http but netlify only allow https sites are:
-          <ul>
-            <li>Marvel</li>
-            <li>Weather</li>
-          </ul>
-        </p>
         </>
     )
 }
